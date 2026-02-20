0347bdfb4a5af7379df74669b02fb0aaa2d326b4
Josefine <josefineeriksson@live.se>
2026-02-20 21:09:15 +0100

    loggo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,17 @@
-import Image from "next/image";
+// import Image from "next/image";
 export default function Header() {
   return (
     <header className="text-center w-full">
         <h1 className="text-3xl sm:text-4xl md:text-5xl font-black text-[#4279b8] mb-3 sm:mb-4 tracking-tight">
         🌤 Kläder efter Väder
       </h1>
-      <Image
+      {/* <Image
         src="/kladereftervader.png"
         alt="Väder ikonen"
         width={300}
         height={300}
         className="mx-auto mb-4"
-      />
+      /> */}
     
 
       <p className="text-base sm:text-lg md:text-xl font-bold bg-[#51b1e8] text-[#ffffff] py-3 sm:py-4 px-5 sm:px-6 rounded-3xl leading-snug shadow-sm">
