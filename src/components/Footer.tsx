192f06a7f2fdb771ab2a7ed8426c76d41e0ad500
Josefine <josefineeriksson@live.se>
2026-02-20 20:34:37 +0100

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,11 @@ export default function Footer() {
   return (
     <footer className="w-full text-center py-6 mt-2 text-[#475569] text-xs sm:text-sm">
       © 2025 Kod och Design av Josefine Eriksson <a href="https://kodochdesign.se" className="underline hover:text-[#1e293b] transition">kodochdesign.se</a>
+      <div className="mt-2">
+        <p className="text-xs sm:text-sm">
+          Webbapplikationen är under utveckling och jag planerar att lansera en mobilapp inom kort.
+        </p>
+      </div>
     </footer>
   );
 }
