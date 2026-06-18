c94fc8da6ec77521d58ffec9d1815ebb70a9ddd7
Josefine <josefineeriksson@live.se>
2026-06-18 21:21:34 +0200

    fix in config

--- next.config.ts
+++ next.config.ts
@@ -39,7 +39,7 @@ const nextConfig: NextConfig = {
           },
           {
             key: "Content-Security-Policy",
-            value: "default-src 'self'; script-src 'self'",
+            value: "default-src 'self'; script-src 'self'; connect-src 'self' https://api.openweathermap.org https://openweathermap.org",
           },
         ],
       },
